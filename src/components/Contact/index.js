65a91dd0af900e65ede2b60fa6acb3412fdc91d4
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-10 22:51:53 -0800

    add validation for Contact Form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+import pinkZebra from '../../assets/pics/pinkZebra.png'
 
 const contactContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   height: '50%',
   width: '100%',
   backgroundColor: '#000000',
@@ -24,9 +30,6 @@ const formContainer = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
-  alignItems: 'left',
-  width: '50%',
-  margin: 'auto'
 };
 
 const formComponent = {
@@ -41,8 +44,12 @@ const formComponent = {
 };
 
 const formButton = {
-  width: '50%',
-  margin: 'auto',
+  fontFamily: 'obstacle',
+  fontSize: '50px',
+  color: '#FFFFFF',
+  backgroundImage: `url(${pinkZebra})`,
+  backgroundSize: 'cover',
+  borderRadius: '5em'
 }
 
 function ContactForm() {
@@ -51,11 +58,35 @@ function ContactForm() {
 
   const { name, email, message } = formState;
 
-  function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
-  }
+  const [errorMessage, setErrorMessage] = useState('');
 
-  console.log(formState);
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid condition statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid!');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    
+    if(!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value })
+    }
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
 
   return(
     <section style={contactContainer}>
@@ -63,17 +94,22 @@ function ContactForm() {
       <form id="contact-form" style={formContainer}>
         <div style={formComponent}>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+          <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
         </div>
         <div style={formComponent}>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+          <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
         </div>
         <div style={formComponent}>
           <label htmlFor="message">Message:</label><br />
-          <textarea type="message" defaultValue={message} onChange={handleChange} rows="5" style={{resize: 'none'}}/>
+          <textarea type="message" defaultValue={message} onBlur={handleChange} rows="5" style={{resize: 'none'}}/>
         </div>
-        <button type="submit" style={formButton}>Let's Connect!</button>
+        {errorMessage && (
+          <div style={formComponent}>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit" onSubmit={handleSubmit} style={formButton}>Let's Connect!</button>
       </form>
     </section>
   )
